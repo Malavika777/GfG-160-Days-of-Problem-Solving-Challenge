6cca6f44de6f62fb8494ffd3f0589ccd7ed6a53c
Malavika777 <131330711+Malavika777@users.noreply.github.com>
2024-12-30 11:58:54 +0530

    Create Union of Arrays with Duplicates.java

---
+++ Union of Arrays with Duplicates.java
@@ -0,0 +1,9 @@
+class Solution {
+    public static int findUnion(int a[], int b[]) {
+        // code here
+        Set<Integer>set=new HashSet<>();
+        for(int i:a) set.add(i);
+        for(int i:b) set.add(i);
+        return set.size();
+    }
+}
