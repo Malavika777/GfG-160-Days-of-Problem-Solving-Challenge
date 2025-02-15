62f9d474e2180c0a19a495661c9083038956f379
MALAVIKA SRINIVASAN <131330711+Malavika777@users.noreply.github.com>
2025-02-15 12:10:04 +0530

    Create Lowest Common Ancestor in a BST.java

---
+++ Lowest Common Ancestor in a BST.java
@@ -0,0 +1,13 @@
+class Solution {
+    Node LCA(Node root, Node n1, Node n2) {
+        // your code here.
+        while(root!=null){
+            if(root.data>n1.data&&root.data>n2.data)
+                root=root.left;
+            else if(root.data<n1.data&&root.data<n2.data)
+                root=root.right;
+            else break;
+        }
+        return root;
+    }
+}
